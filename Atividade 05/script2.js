8e491db09d5aefac86e5aa14109123308385e726
sophiadis <Sophia.dis@icloud.com>
2025-09-30 11:46:51 -0300

    Add files via upload

---
+++ Atividade 05/script2.js
@@ -0,0 +1,5 @@
+//arrays -> vetores, matriz, matrizes 
+// estrutura de dados para armazenar um conjunto de elementos
+
+let frutas = ["Banana", "Maça", "Melancia"]; //array de tamanho 3
+console.log(frutas);
